645bf7846d9472a548d52a864f484df4637eb94f
luismazo <luis.mzo@hotmail.com>
2025-05-23 00:11:16 -0300

    Primer commit del proyecto - Test de conexion a la db y configuracion de variables de entorno

---
+++ src/main/java/org/gimnasioApp/connection/ConnectionDB.java
@@ -0,0 +1,39 @@
+package org.gimnasioApp.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class ConnectionDB {
+    public static Connection getConnection(){
+        Connection connection = null;
+
+        Dotenv dotenv = Dotenv.load();
+
+        String dbName = dotenv.get("DB_NAME");
+        String user = dotenv.get("DB_USER");
+        String pass = dotenv.get("DB_PASS");
+        String baseUrl = dotenv.get("DB_URL");
+        String url = baseUrl + "/" + dbName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url,user,pass);
+
+        }catch (Exception e){
+            System.out.println("ERROR en la conexion a la base de datos " + e);
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        //Test de conexion
+        var conexion = ConnectionDB.getConnection();
+        if( conexion != null ){
+            System.out.println("Conexion exitosa " + conexion);
+        }else{
+            System.out.println("Conexion no establecida");
+        }
+    }
+
+}
